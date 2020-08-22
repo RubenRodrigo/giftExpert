5489b152d5c36152670af41474f559a3b5ff5904
Tkmiz <ruben.chani@tecsup.edu.pe>
2020-08-21 23:05:52 -0500

    Docs agregado

---
+++ docs/precache-manifest.0659a8176afd5320d855f5a11dc3002a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308b6484b59fcf8a7280d941bc5c1ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f6368f71513f2a764f8",
+    "url": "/static/css/main.88909a1a.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6368f71513f2a764f8",
+    "url": "/static/js/main.043d69ed.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
